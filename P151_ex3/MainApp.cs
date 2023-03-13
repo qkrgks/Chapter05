efbd23d4a86efb393da2666d973055f3bcaf3c34
한 박 <djakrma0053@naver.com>
2023-03-13 15:36:25 +0900

    202303131536

---
+++ P151_ex3/MainApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P151_ex3
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("숫자를 입력하세요 : ");
+            //string input = Console.ReadLine();
+            //int number = Convert.ToInt32(input);
+
+            //if (number >= 50)
+            //{
+
+            //    Console.WriteLine($"입력하신 숫자 " + $"{number}는(은) 50이상 입니다.");
+
+            //}
+
+            //else if (number < 50)
+            //{
+            //    Console.WriteLine($"입력하신 숫자 " + $"{number}는(은) 50미만 입니다.");
+            //}
+            
+
+        }
+    }
+}
---
+++ p151_ex2/MainApp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p151_ex2
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("점수를 입력하세요 : ");
+
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+
+
+            if (number >= 90)
+            {
+                Console.WriteLine($"입력하진 점수 " + $"{number}는(은) A 학점 입니다.");
+            }
+
+            else if (number >= 80)
+            {
+                Console.WriteLine($"입력하진 점수 " + $"{number}는(은) B 학점 입니다.");
+            }
+
+            else if (number >= 70)
+            {
+                Console.WriteLine($"입력하진 점수 " + $"{number}는(은) C 학점 입니다.");
+            }
+
+            else 
+            { 
+            Console.WriteLine($"입력하진 점수 " + $"{number}는(은) F 학점 입니다.");
+            }
+
+            //if (number > 0)
+            {
+                //if (number >= 90)
+                    //Console.WriteLine("A");
+               // else if (number >= 80)
+                   // Console.WriteLine("B");
+               // else if (number >= 70)
+                   // Console.WriteLine("C");
+               // else (number <= 69)
+                   // Console.WriteLine("F");
+            }
+            
+                    
+
+        }
+    }
+}
