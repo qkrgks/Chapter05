fc3f03a71e5b1f2befe6e5aca7eac41e567932a6
한 박 <djakrma0053@naver.com>
2023-03-16 15:42:40 +0900

    202303161542

---
+++ p169_ex1/MainApp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p169_ex1
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
